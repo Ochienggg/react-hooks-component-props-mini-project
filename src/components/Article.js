b8cd6487c978a06d8dcf8588d7c90ff3c4147c34
ochieng <ochieng.oduor@student.moringaschool.com>
2025-04-20 23:31:47 +0300

    complete lab

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Article = ({title,date,preview}) => {
+    const defaultDate = "January 1, 1970"
+    const postDate = date || defaultDate
+  return (
+    <article>
+        <h3>{title}</h3>
+        <small>{postDate}</small>
+        <p>{preview}</p>
+    </article>
+      
+    
+  )
+}
+
+export default Article
